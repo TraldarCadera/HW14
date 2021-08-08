2de3ca83edc3441061a09a2e35a16b2f330bc42f
Aleksey Markin <traldar@mail.ru>
2021-08-08 12:03:50 +0300

    Выгруз ДЗ

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+
 public class Task1 {
+
+    public static int[] AfterLast4(int[] arr) throws RuntimeException {
+        int n = 0;
+        String str = Arrays.toString(arr);
+        if (str.contains(String.valueOf(4))) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == 4) n = i;
+            }
+            int[] arr1 = new int[arr.length - n - 1];
+            System.arraycopy(arr, n + 1, arr1, 0, arr1.length);
+            return arr1;
+        } else {
+            throw new RuntimeException("4 не найдена");
+        }
+    }
 }
--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,4 +1,12 @@
 public class Task2 {
 
-
+    public static boolean Only1And4(int[] arr) {
+        boolean i1 = false, i4 = false;
+        for (int j : arr) {
+            if (j == 1) i1 = true;
+            else if (j == 4) i4 = true;
+            else return false;
+        }
+        return i1 && i4;
+    }
 }
